f9bd7ff7f1c39342dd924a0bde0744460f985050
Edgard Tellez\Admin <osais1000@gmail.com>
2025-03-21 15:05:03 -0600

    a

--- app.py
+++ app.py
@@ -7,17 +7,16 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    qr_image = None  # Inicializamos como None
-
+    qr_image = None
     if request.method == "POST":
         url = request.form["url"]
-        # Generar el QR
         img = qrcode.make(url)
-        buffered = BytesIO()
-        img.save(buffered, format="PNG")
-        qr_image = base64.b64encode(buffered.getvalue()).decode("utf-8")  # Convertir la imagen a base64
-    
+        # Guardar la imagen QR en un buffer
+        img_byte_arr = BytesIO()
+        img.save(img_byte_arr)
+        img_byte_arr.seek(0)
+        qr_image = base64.b64encode(img_byte_arr.getvalue()).decode('utf-8')
     return render_template("index.html", qr_image=qr_image)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
